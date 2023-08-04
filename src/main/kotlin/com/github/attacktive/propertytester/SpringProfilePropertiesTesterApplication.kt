b3270a952ae0a0693daf762ad2bd453fd1014617
Attacktive <66462458+Attacktive@users.noreply.github.com>
2023-08-04 12:14:42 +0900

    Initial commit

---
+++ src/main/kotlin/com/github/attacktive/propertytester/SpringProfilePropertiesTesterApplication.kt
@@ -0,0 +1,11 @@
+package com.github.attacktive.propertytester
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringProfilePropertiesTesterApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringProfilePropertiesTesterApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/attacktive/propertytester/SpringProfilePropertiesTesterApplicationTests.kt
@@ -0,0 +1,10 @@
+package com.github.attacktive.propertytester
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringProfilePropertiesTesterApplicationTests {
+	@Test
+	fun contextLoads() {}
+}
