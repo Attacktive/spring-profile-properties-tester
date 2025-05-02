ffcb7ce3d79b14a4a8420da422d116df65b11de8
Attacktive <tribute2pantera@gmail.com>
2025-05-02 15:08:41 +0900

    refactor: use constructor injection instead of `@Autowired` `lateinit var`

--- src/main/kotlin/com/github/attacktive/propertytester/SpringProfilePropertiesTesterApplication.kt
+++ src/main/kotlin/com/github/attacktive/propertytester/SpringProfilePropertiesTesterApplication.kt
@@ -1,19 +1,13 @@
 package com.github.attacktive.propertytester
 
 import com.github.attacktive.propertytester.printer.PrintingService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringProfilePropertiesTesterApplication: CommandLineRunner {
-	@Autowired
-	private lateinit var printingService: PrintingService
-
-	override fun run(vararg args: String?) {
-		printingService.printProperties()
-	}
+class SpringProfilePropertiesTesterApplication(private val printingService: PrintingService): CommandLineRunner {
+	override fun run(vararg args: String?) = printingService.printProperties()
 }
 
 fun main(args: Array<String>) {
