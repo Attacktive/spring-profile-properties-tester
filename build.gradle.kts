42b2d4fc8ddbecefbce4c5f8626d4bfcbbe3ceea
Attacktive <tribute2pantera@gmail.com>
2025-06-24 16:25:57 +0900

    refactor: introduce local variable to avoid repetition

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	val kotlinGradlePluginVersion = "2.2.0"
+
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("jvm") version "2.2.0"
-	kotlin("plugin.spring") version "2.2.0"
+	kotlin("jvm") version kotlinGradlePluginVersion
+	kotlin("plugin.spring") version kotlinGradlePluginVersion
 }
 
 group = "xyz.github.attacktive"
